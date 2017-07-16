0036f05329461d314cbe4f7742896ac1dfb943b3
Niima <nima.hashemy@gmail.com>
2017-07-16 19:45:12 +0430

    first controller

--- controllers/hello.go
+++ controllers/hello.go
@@ -6,6 +6,20 @@ import (
 	"github.com/labstack/echo"
 )
 
-func echoHello(c echo.Context) error {
-	return c.JSON(http.StatusOK, "ok")
+type person struct {
+	name  string
+	Email string
+}
+
+//EchoHello is test
+func EchoHello(c echo.Context) error {
+	// return c.JSON(http.StatusOK, "ok")
+	p := person{name: "hi", Email: "go"}
+	return c.JSON(http.StatusOK, p)
+}
+
+//HelloName is hello
+func HelloName(c echo.Context) error {
+	name := c.QueryParam("name")
+	return c.String(http.StatusOK, "hello "+name)
 }
