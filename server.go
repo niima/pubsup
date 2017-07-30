797a534b1b6af9b61c558b36fce61f140340fe6b
Niima <nima.hashemy@gmail.com>
2017-07-30 13:18:39 +0430

    echo handler for post

--- server.go
+++ server.go
@@ -1,6 +1,11 @@
 package main
 
-import "gitlab.pec.ir/cloud/sync-service/logic"
+import (
+	"os"
+
+	"github.com/labstack/echo"
+	"gitlab.pec.ir/cloud/sync-service/config"
+)
 
 // "os"
 
@@ -9,24 +14,25 @@ import "gitlab.pec.ir/cloud/sync-service/logic"
 //"gitlab.pec.ir/cloud/sync-service/db"
 
 func main() {
-	// e := echo.New()
-	// config.RegisterMiddlewares(e)
-	// config.RegisterRoutes(e)
+	e := echo.New()
+	config.RegisterMiddlewares(e)
+	config.RegisterRoutes(e)
 
-	// //set default port or load frop env
-	// port := "9002"
-	// if os.Getenv("PORT") != "" {
-	// 	port = os.Getenv("PORT")
-	// }
-	// e.Logger.Fatal(e.Start(":" + port))
+	//set default port or load frop env
+	port := "9002"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 
-	//------------bolt
+	// //------------bolt
 	// db.SetData([]byte("hi"), []byte("bye"))
 	// data, err := db.GetData([]byte("hi"))
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
 	// fmt.Println(data)
-	logic.Producer("event", []byte("hi im nima"))
-	logic.Consumer()
+	// //------------nsq
+	// logic.Producer("event", []byte("hi im nima"))
+	// logic.Consumer()
 }
