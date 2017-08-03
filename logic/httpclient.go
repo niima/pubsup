de53bff0229b596605eb1b297db222cde18df444
Niima <nima.hashemy@gmail.com>
2017-08-03 15:07:52 +0430

    http client logic

--- logic/httpclient.go
+++ logic/httpclient.go
@@ -8,7 +8,7 @@ import (
 )
 
 //HTTPPost post data to client
-func HTTPPost(url string, postData []byte) {
+func HTTPPost(url string, postData []byte) error {
 	fmt.Println("URL:>", url)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(postData))
@@ -19,7 +19,7 @@ func HTTPPost(url string, postData []byte) {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	defer resp.Body.Close()
@@ -28,5 +28,5 @@ func HTTPPost(url string, postData []byte) {
 	fmt.Println("response Headers:", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
-
+	return nil
 }
