e6b858a8e5042c0dd129760202883f2d37985218
Niima <nima.hashemy@gmail.com>
2017-08-05 21:59:41 +0430

    load consumers from json config file

---
+++ logic/config.go
@@ -0,0 +1,12 @@
+package logic
+
+import "gitlab.pec.ir/cloud/sync-service/models"
+import "gitlab.pec.ir/cloud/sync-service/db"
+
+const fILEADDRESS = "./config.json"
+
+//LoadConfig loads config from json file
+//retunrs subscriber model
+func LoadConfig() []models.Subscriber {
+	return db.LoadConfiguration(fILEADDRESS)
+}
